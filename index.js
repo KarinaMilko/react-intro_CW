9cb0c488558d4cfc71f02c56961d1272c86c1b0c
Karina <milkokarina13@gmail.com>
2024-07-07 01:43:45 +0300

    Add props

--- index.js
+++ index.js
@@ -22,114 +22,173 @@ const root = ReactDOM.createRoot(rootEl);
 root.render(<App />);
 // root.render(<App></App>);
 
+// function App() {
+//   // return <div>Hello React</div>;
+//   return (
+//     <>
+//       <Header />
+//       <main>
+//         <UserCard />
+//         <News />
+//       </main>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// function News() {
+//   const news = {
+//     title: "Some news",
+//     body: "Something hapenned",
+//     isGood: false,
+//   };
+
+//   const newsStyle = {
+//     border: `3px solid ${news.isGood ? "green" : "red"}`,
+//   };
+
+//   return (
+//     <article style={newsStyle}>
+//       <h2>{news.title}</h2>
+//       <p>{news.body}</p>
+//     </article>
+//   );
+// }
+
+// function UserCard() {
+//   const user = {
+//     name: "Ivo",
+//     age: 19,
+//     isMale: false,
+//     imgSrc:
+//       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+//     favouriteColor: "orange",
+//   };
+
+//   const nameStyle = {
+//     color: "red",
+//     backgroundColor: "green",
+//     border: "2px solid yellow",
+//   };
+
+//   const imgBorderStyle = {
+//     border: `2px solid ${user.favouriteColor}`,
+//   };
+
+//   const ageStyle = {
+//     color: user.age >= 18 ? "blue" : "green",
+//   };
+
+//   // додати alt до img
+//   return (
+//     <article className="userCard">
+//       {/* <h2 className="userName">{user.name}</h2>
+//       <img className="userImg" src={user.imgSrc} alt={user.name} />
+//       <p className="userAge">{user.age}</p> */}
+//       <h2 className="userName" style={nameStyle}>
+//         {user.name}
+//       </h2>
+//       <img
+//         className="userImg"
+//         style={imgBorderStyle}
+//         src={user.imgSrc}
+//         alt={user.name}
+//       />
+//       <p className="userAge" style={ageStyle}>
+//         {user.age}
+//       </p>
+//     </article>
+//   );
+// }
+
+// // Реалізувати компонент (розмітка  + інлайн-стилі)
+// const news = {
+//   title: "Some news",
+//   body: "Something hapenned",
+//   isGood: true,
+// };
+// // гарна новина - зелена, не гарна - червона
+
+// function Header() {
+//   return (
+//     <header className="pageHeader">
+//       <img
+//         className="logoImg"
+//         src="https://cdn-icons-png.flaticon.com/512/5332/5332306.png"
+//         alt="logo"
+//       ></img>
+//       <h1 className="pageTitle">Page Title</h1>
+//     </header>
+//   );
+// }
+
+// // Додати та стилізувати Footer (Contacts, About, FAQ)
+// // (опустити вниз сторінки)
+
+// function Footer() {
+//   return (
+//     <footer className="pageFooter">
+//       <a href="#">Contact</a>
+//       <a href="#">About</a>
+//     </footer>
+//   );
+// }
+//----------------------------------------------------------------------------------------
+// <></> - react fragment (для компонування кількох елементів)
+// class => className = "class1 class2"
+// атрибути іменуються в camelCase
+// style - object
+//    імена властивослей - в camelCase
+//    backgroundColor: 'yellow',
+// function App() {
+//   const news = [
+//     {
+//       title: "Some news1",
+//       body: "Something hapenned1",
+//       isGood: false,
+//     },
+//     {
+//       title: "Some news2",
+//       body: "Something hapenned2",
+//       isGood: true,
+//     },
+//   ];
+//   return (
+//     <>
+//       <News newsItem={news[0]} />
+//       <News newsItem={news[1]} />
+//     </>
+//   );
+// }
+// // News(news[0])
+// // News(news[1])
+// // <News newsItem={news[0]} /> => News({newsItem:news[0]})
+
+// function News(props) {
+//   const news = props.newsItem;
+//   const newsStyle = {
+//     border: `3px solid ${news.isGood ? "green" : "red"}`,
+//   };
+
+//   return (
+//     <article style={newsStyle}>
+//       <h2>{news.title}</h2>
+//       <p>{news.body}</p>
+//     </article>
+//   );
+// }
 function App() {
-  // return <div>Hello React</div>;
-  return (
-    <>
-      <Header />
-      <main>
-        <UserCard />
-        <News />
-      </main>
-      <Footer />
-    </>
-  );
+  const isGreeting = false;
+  const userName = "Test";
+  return <Greeting isGreeting={isGreeting} userName={userName} />;
 }
 
-function News() {
-  const news = {
-    title: "Some news",
-    body: "Something hapenned",
-    isGood: false,
-  };
-
-  const newsStyle = {
-    border: `3px solid ${news.isGood ? "green" : "red"}`,
-  };
-
+// додати змінну з іменем користувача і прокинути в компонент
+function Greeting(props) {
+  const { isGreeting, userName } = props;
   return (
-    <article style={newsStyle}>
-      <h2>{news.title}</h2>
-      <p>{news.body}</p>
-    </article>
-  );
-}
-
-function UserCard() {
-  const user = {
-    name: "Ivo",
-    age: 19,
-    isMale: false,
-    imgSrc:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-    favouriteColor: "orange",
-  };
-
-  const nameStyle = {
-    color: "red",
-    backgroundColor: "green",
-    border: "2px solid yellow",
-  };
-
-  const imgBorderStyle = {
-    border: `2px solid ${user.favouriteColor}`,
-  };
-
-  const ageStyle = {
-    color: user.age >= 18 ? "blue" : "green",
-  };
-
-  // додати alt до img
-  return (
-    <article className="userCard">
-      {/* <h2 className="userName">{user.name}</h2>
-      <img className="userImg" src={user.imgSrc} alt={user.name} />
-      <p className="userAge">{user.age}</p> */}
-      <h2 className="userName" style={nameStyle}>
-        {user.name}
-      </h2>
-      <img
-        className="userImg"
-        style={imgBorderStyle}
-        src={user.imgSrc}
-        alt={user.name}
-      />
-      <p className="userAge" style={ageStyle}>
-        {user.age}
-      </p>
-    </article>
-  );
-}
-
-// Реалізувати компонент (розмітка  + інлайн-стилі)
-const news = {
-  title: "Some news",
-  body: "Something hapenned",
-  isGood: true,
-};
-// гарна новина - зелена, не гарна - червона
-
-function Header() {
-  return (
-    <header className="pageHeader">
-      <img
-        className="logoImg"
-        src="https://cdn-icons-png.flaticon.com/512/5332/5332306.png"
-        alt="logo"
-      ></img>
-      <h1 className="pageTitle">Page Title</h1>
-    </header>
-  );
-}
-
-// Додати та стилізувати Footer (Contacts, About, FAQ)
-// (опустити вниз сторінки)
-
-function Footer() {
-  return (
-    <footer className="pageFooter">
-      <a href="#">Contact</a>
-      <a href="#">About</a>
-    </footer>
+    <div>
+      {isGreeting ? "Hello" : "Goodbye"}, {userName}
+    </div>
   );
 }
